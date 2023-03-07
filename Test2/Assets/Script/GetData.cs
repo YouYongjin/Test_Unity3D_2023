1b0146ce863b8f60fd22a801db8cae6487135bde
radio020112@gmail.com <@dydwls2002>
2023-03-07 14:47:38 +0900

    Excel Data 읽어서 Debug Log 표시.
    제목이 곧 내용

---
+++ Test2/Assets/Script/GetData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetData : MonoBehaviour
+{
+    public Entity_GameDB entity_GameDB;
+    void Start()
+    {
+        foreach(Entity_GameDB.Param param in entity_GameDB.sheets[0].list)
+        {
+            Debug.Log(param.index + " - " + param.name + " - " + param.hp + " - " + param.mp);
+        }
+    }
+}
